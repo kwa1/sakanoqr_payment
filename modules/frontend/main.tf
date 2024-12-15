2703536ab2b9937ac3c322f37877376b336589ec
Dennis Amankwa <amankwa32@hotmail.com>
2024-12-15 22:39:05 +0000

    Create main.tf

---
+++ modules/frontend/main.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = var.bucket_name
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+output "frontend_url" {
+  value = aws_s3_bucket.frontend_bucket.website_endpoint
+}
